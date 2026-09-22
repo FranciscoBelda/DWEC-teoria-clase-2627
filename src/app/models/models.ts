5a20fce25131126e4a86d8d30a0feb0b926b3e15
FranciscoBelda <francisco.belda@cieep.com>
2026-09-22 10:49:28 +0200

    Tema 4 - Ejercicio 3.2

--- src/app/models/models.ts
+++ src/app/models/models.ts
@@ -1,3 +1,5 @@
+import { User } from '../common/interfaces';
+
 export type CategoryType = 'electronics' | 'clothing' | 'books' | 'home';
 
 export interface Rating{
@@ -13,4 +15,16 @@ export interface Product {
   category: CategoryType;
   imageUrl: string;
   rating?: Rating;
+}
+export interface CartItem{
+  product: Product;
+  quantity: number;
+}
+export type PayMethod = 'credit_card' | 'paypal';
+export interface Order {
+  id: string;
+  cartItems: CartItem[];
+  totalPrice: number;
+  payMethod: PayMethod;
+  user: User;
 }
